1daf6f941b0646a4596a8f5492de9d03f1d44481
Nathaniel Louis Tisuela <natetisuela@gmail.com>
2020-10-23 00:34:10 -0700

    improved algorithm

--- tweetbrain/backend/app/api_v1/songs/utils.py
+++ tweetbrain/backend/app/api_v1/songs/utils.py
@@ -5,11 +5,35 @@ match = Match()
 def match_with_top_words(twitter_handle: str):
     user = TwitterUser(twitter_handle)
     top_words = user.get_top_words()
-    songs = match.search_songs(top_words[0][0])
+    songs = list()
+
+    # list of words that return a song
+    valid_words = list()
+    valid_words_str = ""
+    max_valid_words = 7
+
+    index = 0
+    max_index = 7
+    while (len(valid_words_str.split()) < max_valid_words) and (index < max_index):
+        songs.clear()
+        query = f"{valid_words_str} {top_words[index][0]}"
+        results = match.search_songs(query)
+
+        for result in results["hits"]:
+            if (result["type"] =="song"):
+                songs.append(result["result"])
+
+        if len(songs) > 0:
+            valid_words_str = query
+
+
+        index += 1
+
+
 
 
     results = list()
 
-    for song in songs["hits"]:
-        results.append(song["result"])
-    return results
+
+    print(valid_words_str, "WOWOWOWOWOW \n\n\n\n")
+    return songs
