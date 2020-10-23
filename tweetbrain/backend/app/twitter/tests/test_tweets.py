9a152e3857b049319e1ba7aa7a254583999e23c3
codemanrj <rmontano11@student.mtsac.edu>
2020-10-22 21:36:12 -0700

    Added hashtag function

--- tweetbrain/backend/app/twitter/tests/test_tweets.py
+++ tweetbrain/backend/app/twitter/tests/test_tweets.py
@@ -1,14 +1,15 @@
 from ..twitter_user import TwitterUser
 
 
-user = TwitterUser("MontellFish")
-def test_get_user():
+user = TwitterUser("lopez__11")
 
+def test_get_user():
     print(user.num_of_tweets)
 
-
+def test_hashtags():
+    print(user.get_top_hastags())
 
 def test_get_top_words():
-    user = TwitterUser("MontellFish")
     top = user.get_top_words()
     print(top)
+    print('\n')
